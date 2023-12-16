9d3352d90f968a41c7b2925d80bd239a126d9d54
Dslayton1998 <dslayton1998@gmail.com>
2023-12-15 18:51:51 -0600

    started on the Album frontend

--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import LoginFormPage from '../components/LoginFormPage';
 import SignupFormPage from '../components/SignupFormPage';
+import AllAlbums from '../components/AlbumComponents/AllAlbums';
 import Layout from './Layout';
 
 export const router = createBrowserRouter([
@@ -19,6 +20,10 @@ export const router = createBrowserRouter([
         path: "signup",
         element: <SignupFormPage />,
       },
+      {
+        path: "album",
+        element: <AllAlbums />
+      }
     ],
   },
 ]);
