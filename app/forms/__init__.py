234369f5ec92cf4575d87fc42562e464428e20ec
Donald Roessler <me@donaldroessler.com>
2023-12-18 13:54:07 -0800

    Song routes

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -1,3 +1,4 @@
 from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .album_form import AlbumForm
+from .song_form import SongForm, SongUpdateForm
---
+++ app/forms/song_form.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileRequired, FileAllowed
+from wtforms import StringField, FileField, IntegerField
+from wtforms.validators import DataRequired
+
+ALLOWED_EXTENSIONS = {"mp3", "ogg", "wav"}
+
+class SongForm(FlaskForm):
+  title = StringField("Title", validators=[DataRequired()])
+  album_id = IntegerField("Album Id", validators=[DataRequired()])
+  song_file = FileField("Song File", validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))])
+
+class SongUpdateForm(FlaskForm):
+  title = StringField("Title")
+  song_file = FileField("Song File", validators=[FileAllowed(list(ALLOWED_EXTENSIONS))])
