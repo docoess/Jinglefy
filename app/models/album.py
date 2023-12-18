7201cb97ea8fba13e0db94494c8b0d95dcd8e881
Donald Roessler <me@donaldroessler.com>
2023-12-18 09:30:38 -0800

    Update for initial deployment

--- app/models/album.py
+++ app/models/album.py
@@ -1,12 +1,20 @@
-from .db import db
+from .db import db, add_prefix_for_prod
+import os
+
+environment = os.getenv('FLASK_ENV')
+SCHEMA = os.environ.get('SCHEMA')
 
 class Album(db.Model):
     __tablename__ = 'albums'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50), nullable=False)
     cover_image = db.Column(db.String(255), nullable=False)
     desc = db.Column(db.String(2000), nullable=False)
-    artist_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    artist_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")))
     num_songs = db.Column(db.Integer, nullable=False)
     release_date = db.Column(db.Date, nullable=False)
 
