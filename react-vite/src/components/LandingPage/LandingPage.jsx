f168e1e676c1f25a3644c54025879928c20b240f
Donald Roessler <me@donaldroessler.com>
2023-12-20 16:34:24 -0800

    some styling

--- react-vite/src/components/LandingPage/LandingPage.jsx
+++ react-vite/src/components/LandingPage/LandingPage.jsx
@@ -1,13 +1,14 @@
 import { NavLink } from "react-router-dom";
 import "../AllAlbums/AllAlbums.css";
+import './LandingPage.css'
 
 export default function LandingPage() {
     return (
-        <>
-            <h1>Welcome to Jinglefy!</h1>
+        <div className="landing-page-container">
+            <h1 className="landing-h1" >Welcome to Jinglefy!</h1>
             <h2>Made by Azad Celik, Derek Slayton, Donald Roessler, and Elliot Starr</h2>
 
             <NavLink to='/albums'>View all albums</NavLink>
-        </>
+        </div>
     )
 }
