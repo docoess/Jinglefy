5097fb3acbf0c83982d7ed192b064156dfbac708
Elliot <Heecher626@gmail.com>
2023-12-21 15:54:07 -0500

    favicon, github links, num_songs update on delete

--- react-vite/src/components/LandingPage/LandingPage.jsx
+++ react-vite/src/components/LandingPage/LandingPage.jsx
@@ -6,7 +6,7 @@ export default function LandingPage() {
     return (
         <div className="landing-page-container">
             <h1 className="landing-h1" >Welcome to Jinglefy!</h1>
-            <h2>Made by Azad Celik, Derek Slayton, Donald Roessler, and Elliot Starr</h2>
+            <h2>Made by <a href="https://github.com/Azadcelik">Azad Celik</a>, <a href="https://github.com/Dslayton1998">Derek Slayton</a>, <a href="https://github.com/docoess">Donald Roessler</a>, and <a href="https://github.com/Heecher626">Elliot Starr</a></h2>
 
             <NavLink to='/albums'>View all albums</NavLink>
         </div>
