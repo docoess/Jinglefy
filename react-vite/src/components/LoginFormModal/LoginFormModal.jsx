5a47a099179158d34a7122fd1295e456b751746f
Donald Roessler <me@donaldroessler.com>
2024-01-29 10:42:08 -0800

    Sort albums by track num. Redirect to albums on login

--- react-vite/src/components/LoginFormModal/LoginFormModal.jsx
+++ react-vite/src/components/LoginFormModal/LoginFormModal.jsx
@@ -26,7 +26,7 @@ function LoginFormModal() {
     if (serverResponse) {
       setErrors(serverResponse);
     } else {
-      navigate('/')
+      navigate('/albums')
       closeModal();
     }
   };
@@ -43,7 +43,7 @@ function LoginFormModal() {
     if(serverResponse){
       setErrors(serverResponse)
     } else {
-      navigate('/')
+      navigate('/albums')
       closeModal()
     }
   }
